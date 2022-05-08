6ec5f9710411d688ab190ea8dcac5c7b805e1e04
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-05-08 17:30:32 +0300

    add link to="" and description at admin/order

--- src/pages/Admin/Orders/index.js
+++ src/pages/Admin/Orders/index.js
@@ -1,61 +1,71 @@
-import React from 'react';
-import {useQuery} from "react-query";
-import {fetchOrder} from "../../../api";
+import React from "react";
+import { useQuery } from "react-query";
+import { fetchOrder } from "../../../api";
 import {
-	Table,
-	Thead,
-	Tbody,
-	Tr,
-	Th,
-	Td,
-	TableCaption,
-	Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  Text,
 } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 function Orders() {
   // catch'lenirken ne ile catch olacak (verilen data), o yüzden useQuery'e key veriyoruz.
-  const {data, isLoading, isError , error} = useQuery("admin:orders", fetchOrder);
+  const { data, isLoading, isError, error } = useQuery(
+    "admin:orders",
+    fetchOrder
+  );
 
-  if(isLoading) {
-    return <div>Loading...</div>
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
-  if(isError) {
-    return <div>Error {error.message}</div>
+  if (isError) {
+    return <div>Error {error.message}</div>;
   }
 
   return (
     <div>
-       <Text fontSize="2xl" p="5">Siparişler</Text>
+      <Text fontSize="2xl" p="5">
+        Siparişler
+      </Text>
 
-       <Table variant="simple" fontSize="md">
-         <TableCaption></TableCaption>
-         <Thead>
-           <Tr>
-             <Th>Kullanıcı</Th>
-             <Th>Adres</Th>
-             <Th isNumeric>Sipariş Detayı</Th>
-           </Tr>
-         </Thead> 
+      <Table variant="simple" fontSize="md">
+        <TableCaption></TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Kullanıcı</Th>
+            <Th>Adres</Th>
+            <Th isNumeric>Sipariş Detayı</Th>
+          </Tr>
+        </Thead>
 
-         <Tbody>
-            {data.map((item) => (
-              <Tr key={item.id}>
-                <Td>{item.email}</Td>
-                <Td>{item.address}</Td>
-                <Td isNumeric>{item.basketItems.map((product) => {
+        <Tbody>
+          {data.map((item) => (
+            <Tr key={item.id}>
+              <Td>{item.email}</Td>
+              <Td>{item.address}</Td>
+              <Td isNumeric>
+                {item.basketItems.map((product) => {
                   return (
                     <div>
-                      {product.title} - {product.price} $
+                      <Link to={`/product/${product.id}`}>
+                        {product.title} ({product.description}) - {product.price} $
+                      </Link>
                     </div>
-                  )
-                })}</Td>
-              </Tr>
-            ))}
-         </Tbody>
-       </Table>
+                  );
+                })}
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </div>
-  )
+  );
 }
 
 export default Orders;

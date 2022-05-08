86043f9b7fd2754fa007d8b077569edc23b0f9f0
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-05-08 17:38:44 +0300

    add CSS changes at product detail page

--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -23,25 +23,33 @@ function ProductDetail() {
 
   return (
     <div>
-      {/* butona tıklayınca sepete ekleyecek, data API'dan gelen data. */}
-      <Button
-        backgroundColor={findBasketItems ? "#cab1bd" : "#b5c9c3"}
-        color="white"
-        onClick={() => addToBasket(data, findBasketItems)}
-      >
-        {findBasketItems ? "Sepetten kaldır" : "Sepete Ekle"}
-      </Button>
-
       <Text as="h2" fontSize="2xl">
         {data[0].title}
       </Text>
 
       {/* <Text>{moment(data[0].createdAt).format("DD/MM/YYYY")}</Text> */}
 
-      <p>{data[0].description}</p>
+      <p>
+        {data[0].description} - {data[0].price}$
+      </p>
+
+      {/* butona tıklayınca sepete ekleyecek, data API'dan gelen data. */}
+      <Button
+        backgroundColor={findBasketItems ? "#cab1bd" : "#b5c9c3"}
+        color="white"
+        mt="1"
+        onClick={() => addToBasket(data, findBasketItems)}
+      >
+        {findBasketItems ? "Sepetten kaldır" : "Sepete Ekle"}
+      </Button>
 
       <Box margin="10">
-        <ImageGallery items={images} />
+        <ImageGallery
+          items={images}
+          showFullscreenButton={false}
+          useBrowserFullscreen={false}
+          autoPlay={false}
+        />
       </Box>
     </div>
   );

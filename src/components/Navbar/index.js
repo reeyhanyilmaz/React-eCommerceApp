f4e1997c186371370153559a62c75aad60214514
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-05-10 14:06:52 +0300

    change Kategoriler menu design

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,19 +3,15 @@ import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import {
   Button,
-  Drawer,
-  DrawerBody,
-  DrawerOverlay,
-  DrawerContent,
-  useDisclosure,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
   MenuDivider,
   MenuGroup,
+  MenuItemOption,
+  MenuOptionGroup,
 } from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
 import { useAuth } from "../../contexts/AuthContext";
 import { useBasket } from "../../contexts/BasketContext";
 import { useNavigate } from "react-router-dom";
@@ -23,7 +19,6 @@ import { useNavigate } from "react-router-dom";
 function Navbar() {
   const { loggedIn, user } = useAuth();
   const { basketItems } = useBasket();
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { logout } = useAuth();
   const navigate = useNavigate();
@@ -38,8 +33,7 @@ function Navbar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.innerContainer}>
-      <div className={styles.left}>
-       
+        <div className={styles.left}>
           <Link to="/">
             <img
               src="/assets/logo.jpg"
@@ -55,115 +49,110 @@ function Navbar() {
               className={styles.logo}
             />
           </Link>
-          
-        <ul className={styles.menu}>
-          <li>
-            <Link to="/">Ana Sayfa</Link>
-          </li>
 
-          <li>
-            <a onClick={onOpen}>Kategoriler<ChevronDownIcon boxSize="1.5em"/></a>
-            <Drawer placement="top" onClose={onClose} isOpen={isOpen}>
-              <DrawerOverlay />
-              <DrawerContent>
-                {/* <DrawerHeader borderBottomWidth="1px">Kategoriler</DrawerHeader> */}
-                <DrawerBody className={styles.drawerMenuLi}>
-                  <ul>
-                    <li>
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/">Ana Sayfa</Link>
+            </li>
+
+            <li>
+              <Link to="/Products/AllProducts">Ürünler</Link>
+            </li>
+
+            <li>
+              <Menu closeOnSelect={true} autoSelect={true}>
+                <MenuButton as={Button} color="#4a5568" variant="outline">
+                  Kategoriler
+                </MenuButton>
+                <MenuList minWidth="240px">
+                  <MenuOptionGroup defaultValue="asc" type="radio">
+                    <MenuItemOption value="desc">
                       <Link to="/Products/CamMalzeme">Cam Malzeme</Link>
-                    </li>
-                  </ul>
+                    </MenuItemOption>
 
-                  <ul>
-                    <li>
-                      <Link to="/Products/Pipet">Otomatik Pipet & Pipet Uçları</Link>
-                    </li>
-                  </ul>
-
-                  <ul>
-                    <li>
+                    <MenuItemOption value="desc">
                       <Link to="/Products/HacimselOlcum">Hacimsel Ölçüm</Link>
-                    </li>
-                  </ul>
+                    </MenuItemOption>
 
-                  <ul>
-                    <li>
-                      <Link to="/Products/LaboratuvarCihazlari">Laboratuvar Cihazları</Link>
-                    </li>
-                  </ul>
-                </DrawerBody>
-              </DrawerContent>
-            </Drawer>
-          </li>
+                    <MenuItemOption value="desc">
+                      <Link to="/Products/Pipet">
+                        Otomatik Pipet & Pipet Uçları
+                      </Link>
+                    </MenuItemOption>
 
-          <li>
-            <Link to="/Products/AllProducts">Ürünler</Link>
-          </li>
-        </ul>
-      </div>
+                    <MenuItemOption value="asc">
+                      <Link to="/Products/LaboratuvarCihazlari">
+                        Laboratuvar Cihazları
+                      </Link>
+                    </MenuItemOption>
+                  </MenuOptionGroup>
+                </MenuList>
+              </Menu>
+            </li>
+          </ul>
+        </div>
 
-      <div className={styles.right}>
-        {/* kullanıcı giris yapmamıssa */}
-        {!loggedIn && (
-          <>
-            <Link to="/signin">
-              <Button color="white" backgroundColor="#c0b9dd">
-                Giriş
-              </Button>
-            </Link>
-
-            <Link to="/signup">
-              <Button color="white" backgroundColor="#c0b9dd">
-                Yeni Üye Girişi
-              </Button>
-            </Link>
-          </>
-        )}
-
-        {/* kullanıcı girisi varsa profile gidecek */}
-        {loggedIn && (
-          <>
-            {/* giris yapılmıssa sepetteki ürünleri göstermek icin */}
-            {basketItems.length > 0 && (
-              <Link to="/basket">
-                <Button color="#4a5568" variant="outline">
-                  Sepetim {basketItems.length}
+        <div className={styles.right}>
+          {/* kullanıcı giris yapmamıssa */}
+          {!loggedIn && (
+            <>
+              <Link to="/signin">
+                <Button color="white" backgroundColor="#c0b9dd">
+                  Giriş
                 </Button>
               </Link>
-            )}
 
-            <Menu>
-              <MenuButton as={Button} ml="1" color="#4a5568" p="5">
-                Profil
-              </MenuButton>
-              <MenuList>
-                <MenuGroup title="Profil" fontSize="15">
-                  <MenuDivider />
-                  <Link to="/profile">
-                    <MenuItem fontSize="16" mb="1">
-                      <a>Hesap Bilgilerim</a>
-                    </MenuItem>
-                  </Link>
+              <Link to="/signup">
+                <Button color="white" backgroundColor="#c0b9dd">
+                  Yeni Üye Girişi
+                </Button>
+              </Link>
+            </>
+          )}
 
-                  {/* admin islemleri */}
-                  {user?.role === "admin" && (
-                    <Link to="/admin/orders">
+          {/* kullanıcı girisi varsa profile gidecek */}
+          {loggedIn && (
+            <>
+              {/* giris yapılmıssa sepetteki ürünleri göstermek icin */}
+              {basketItems.length > 0 && (
+                <Link to="/basket">
+                  <Button color="#4a5568" variant="outline">
+                    Sepetim {basketItems.length}
+                  </Button>
+                </Link>
+              )}
+
+              <Menu>
+                <MenuButton as={Button} ml="1" color="#4a5568" p="5">
+                  Profil
+                </MenuButton>
+                <MenuList>
+                  <MenuGroup title="Profil" fontSize="15">
+                    <MenuDivider />
+                    <Link to="/profile">
                       <MenuItem fontSize="16" mb="1">
-                        <a>Admin</a>
+                        <a>Hesap Bilgilerim</a>
                       </MenuItem>
                     </Link>
-                  )}
 
-                  <MenuItem fontSize="16" mb="1">
-                    <a onClick={handleLogout}>Çıkış Yap</a>
-                  </MenuItem>
-                </MenuGroup>
-              </MenuList>
-            </Menu>
-          </>
-        )}
-      </div>
+                    {/* admin islemleri */}
+                    {user?.role === "admin" && (
+                      <Link to="/admin/orders">
+                        <MenuItem fontSize="16" mb="1">
+                          <a>Admin</a>
+                        </MenuItem>
+                      </Link>
+                    )}
 
+                    <MenuItem fontSize="16" mb="1">
+                      <a onClick={handleLogout}>Çıkış Yap</a>
+                    </MenuItem>
+                  </MenuGroup>
+                </MenuList>
+              </Menu>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );

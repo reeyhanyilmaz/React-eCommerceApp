e0f2d4044504f3aacb2a81cdfc95f09460bb5654
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-05-14 13:06:11 +0300

    add home page grow hover effect at image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,61 +9,61 @@ import { Carousel } from "react-responsive-carousel";
 function Home() {
   return (
     <div>
-      <Grid templateColumns="repeat(8, 1fr)" gap={4} mt="8">
+      <Grid templateColumns="repeat(8, 1fr)" gap={4} mt="3" mb="20">
         {/* <Box borderRadius="8px" width="100px" height="150px" mt="5" size="30px">
           <img src="/assets/logo.jpg" alt="logo" />
         </Box> */}
 
-        <Box borderRadius="8px" width="100px" height="70px">
+        <Box  className={styles.grow} height="50px" width="100px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5yJb-W5ql6Gr34v--7KIOKFYZE13z196IFw&usqp=CAU"
             alt="icon"
           />
         </Box>
 
-        <Box borderRadius="8px" width="100px" height="100px">
+        <Box className={styles.grow} height="70px" width="100px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7TvFv26-q7KMK0o0VrW8HSvJloL56R99Dhg&usqp=CAU"
             alt="icon"
           />
         </Box>
 
-        <Box borderRadius="8px" width="100px" height="100px">
+        <Box className={styles.grow } height="70px" width="100px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbJr_suVyGoY7zcST_eqs4Bu_chZgOi0jlEA&usqp=CAU"
             alt="icon"
           />
         </Box>
 
-        <Box borderRadius="8px" width="100px" height="100px">
+        <Box className={styles.grow} height="70px" width="100px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoUZD777dUP8jf2_zbCvGqBwBuBG3oTeMBbw&usqp=CAU"
             alt="icon"
           />
         </Box>
 
-        <Box borderRadius="8px" width="100px" height="100px">
+        <Box className={styles.grow} height="70px" width="100px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdbWJ95Ka6ZB7kHYNeOcrwYdINEcrblEVbNw&usqp=CAU"
             alt="icon"
           />
         </Box>
 
-        <Box borderRadius="8px" width="100px" height="100px">
+        <Box className={styles.grow} height="70px" width="100px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSblLWaWJW6dn0pngwAKV0zyHTslLB4H4XfVw&usqp=CAU"
             alt="icon"
           />
         </Box>
 
-        <Box borderRadius="8px" width="100px" height="100px">
+        <Box className={styles.grow} height="70px" width="100px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTub-yeliCQld-pTEdDq08AJjeZT1C9nkAQew&usqp=CAU"
             alt="icon"
           />
         </Box>
 
-        <Box borderRadius="8px" width="100px" height="100px">
+        <Box className={styles.grow} height="70px" width="100px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2D8MrjnmzzDDHzZuFat_DIZDA0TBhetGAlQ&usqp=CAU"
             alt="icon"
@@ -80,25 +80,46 @@ function Home() {
           width="800px"
         >
           <div>
-            <img src="https://www.elislab.com.tr/wp-content/uploads/2020/01/laboratuvar-sarf-malzemeleri.jpg" alt="carouselImage" />
+            <img
+              src="https://www.elislab.com.tr/wp-content/uploads/2020/01/laboratuvar-sarf-malzemeleri.jpg"
+              alt="carouselImage"
+            />
           </div>
           <div>
-            <img src="https://microbenotes.com/wp-content/uploads/2020/05/Instruments-used-in-Microbiology-Lab-with-Principle-and-Uses.jpeg" alt="carouselImage"/>
+            <img
+              src="https://microbenotes.com/wp-content/uploads/2020/05/Instruments-used-in-Microbiology-Lab-with-Principle-and-Uses.jpeg"
+              alt="carouselImage"
+            />
           </div>
           <div>
-            <img src="https://www.nasilbe.com/wp-content/uploads/2020/07/Laboratuvar-Malzemelerinin-isimleri-Nelerdir-1024x551.jpg" alt="carouselImage" />
+            <img
+              src="https://www.nasilbe.com/wp-content/uploads/2020/07/Laboratuvar-Malzemelerinin-isimleri-Nelerdir-1024x551.jpg"
+              alt="carouselImage"
+            />
           </div>
           <div>
-            <img src="https://www.sentezlab.com/class/INNOVAEditor/assets/laboratuvar%20cam%20malzemeleri.jpg" alt="carouselImage"/>
+            <img
+              src="https://www.sentezlab.com/class/INNOVAEditor/assets/laboratuvar%20cam%20malzemeleri.jpg"
+              alt="carouselImage"
+            />
           </div>
           <div>
-            <img src="https://www.kocar.com.tr/uploads/slider/images/000/000/049/medium" alt="carouselImage"/>
+            <img
+              src="https://www.kocar.com.tr/uploads/slider/images/000/000/049/medium"
+              alt="carouselImage"
+            />
           </div>
           <div>
-            <img src="https://static.vecteezy.com/system/resources/thumbnails/000/419/993/small/ry2k_k4r5_180620.jpg" alt="carouselImage"/>
+            <img
+              src="https://static.vecteezy.com/system/resources/thumbnails/000/419/993/small/ry2k_k4r5_180620.jpg"
+              alt="carouselImage"
+            />
           </div>
           <div>
-            <img src="https://tetralaboratuvar.com/wp-content/uploads/2021/03/Ozel-Laboratuvar.jpg" alt="carouselImage"/>
+            <img
+              src="https://tetralaboratuvar.com/wp-content/uploads/2021/03/Ozel-Laboratuvar.jpg"
+              alt="carouselImage"
+            />
           </div>
         </Carousel>
       </Box>
@@ -111,6 +132,7 @@ function Home() {
         border="solid 1px #e0e0e0"
         borderRadius="8px"
         paddingTop="10px"
+        className={styles.grow}
       >
         <img
           src="http://sc04.alicdn.com/kf/HTB1zgqmhlmWBuNkSndV763sApXal.png"

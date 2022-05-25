5155177b8d8aa14cbcef4190d1519f19197f138a
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-05-25 11:23:45 +0300

    add map at home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Box, Text, Grid, Button } from "@chakra-ui/react";
 import styles from "./styles.module.css";
-import { IconButton } from "@chakra-ui/react";
-import { EmailIcon, PhoneIcon } from "@chakra-ui/icons";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
 function Home() {
   return (
     <div>
-      <Grid templateColumns="repeat(8, 1fr)"  mt="3" mb="20" className={styles.iconContainer}>
+      <Grid
+        templateColumns="repeat(8, 1fr)"
+        mt="3"
+        mb="20"
+        className={styles.iconContainer}
+      >
         {/* <Box borderRadius="8px" width="100px" height="150px" mt="5" size="30px">
           <img src="/assets/logo.jpg" alt="logo" />
         </Box> */}
@@ -30,7 +33,7 @@ function Home() {
           />
         </Box>
 
-        <Box className={styles.grow } height="70px" width="90px">
+        <Box className={styles.grow} height="70px" width="90px">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbJr_suVyGoY7zcST_eqs4Bu_chZgOi0jlEA&usqp=CAU"
             alt="icon"
@@ -140,18 +143,17 @@ function Home() {
         border="solid 1px #e0e0e0"
         borderRadius="8px"
         paddingTop="10px"
-        className={styles.grow}       
+        className={styles.grow}
       >
         <img
           src="http://sc04.alicdn.com/kf/HTB1zgqmhlmWBuNkSndV763sApXal.png"
           alt="homeImage"
           className={styles.encokSatılanlarImage}
-         
         />
         <img
           src="https://www.nukleonlab.com.tr/images/urunler/Pipet-ucu-780.jpg"
-          alt="homeImage"  
-          className={styles.encokSatılanlarImage}      
+          alt="homeImage"
+          className={styles.encokSatılanlarImage}
         />
         <Box className={styles.enCokSatılanlarBox}>
           <Text
@@ -181,36 +183,39 @@ function Home() {
         />
       </Box>
 
-      <Grid templateColumns="repeat(2, 1fr)" gap={6} mt="20" className={styles.explanationContact}>
+      <Grid
+        templateColumns="repeat(2, 1fr)"
+        gap={6}
+        mt="20"
+        className={styles.explanationContact}
+      >
         <Box className={styles.explanationContact__textBox}>
           <Text fontSize="lg" className={styles.explanation}>
+            <br />
+            <br />
+            <br />
             En kaliteli ve hijyenik malzemelerden yapılan ChemDev Kimyasal
             Malzemeleri ile çalışmalarınızı en başarılı şekilde
             gerçekleştirebileceksiniz. Yenilenen ürün yelpazemizi kaçırmamak
-            için bizi sosyal medya hesaplarımızdan takip edebilirsiniz. Bizimle
-            telefon ve mail yoluyla iletişime geçebilirsiniz.
+            için bizi sosyal medya hesaplarımızdan takip edebilirsiniz. WhatsApp
+            destek hattımızdan ve iletişim kanallarımızdan bizimle telefon ve
+            mail yoluyla iletişime geçebilirsiniz. Bizi ofisimizde ziyaret etmek
+            için lütfen haritayı incelemeyi unutmayınız.
+            <br />
+            <br />
+            <br />
           </Text>
         </Box>
-
-        <Box className={styles.explanationContact__buttonBox}>
-          <IconButton
-            variant="outline"
-            variantColor="#84A59D"
-            color="#84A59D"
-            aria-label="Send email"
-            icon={<EmailIcon />}
-            className={styles.contactPart}
-          />
-
-          <br />
-          <br />
-          <IconButton
-            backgroundColor="#84A59D"
-            color="white"
-            aria-label="Call Segun"
-            icon={<PhoneIcon />}
-            className={styles.contactPart}
-          />
+       
+        <Box className={styles.map}>
+        <iframe
+          src="https://www.google.com.tr/maps/@41.0079044,28.8968487,11.25z?hl=tr"
+          width="100%"
+          height="400"
+          title="İstanbul"
+          loading="lazy"
+          borderRadius="8px"
+        ></iframe>
         </Box>
       </Grid>
     </div>

61d66391eecec5866b21133b60da0b0872d490be
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-08-10 11:54:44 +0300

    fix map

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -212,7 +212,7 @@ function Home() {
        
         <Box className={styles.map}>
         <iframe
-          src="https://www.google.com.tr/maps/@41.0079044,28.8968487,11.25z?hl=tr"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96320.74916655212!2d28.649985440984736!3d41.02474374700395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caa727fbb7a5af%3A0x36e1fd9eda6fa4da!2zQXZjxLFsYXIvxLBzdGFuYnVs!5e0!3m2!1str!2str!4v1660121056193!5m2!1str!2str" 
           width="100%"
           height="400"
           title="İstanbul"
